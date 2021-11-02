b5b9e4bf24a8f26d89859f007e8206b957a4f968
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2021-11-02 18:02:44 -0400

    Create hero section of mobile view

---
+++ components/layout/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav class="w-screen flex items-center justify-center py-2">
+			<h1 class="brand text-sm">YasRch</h1>
+	</nav>
+</template>
+
+<script lang='ts'>
+import {Vue, Component} from 'nuxt-property-decorator'
+
+@Component({})
+export default class Navbar extends Vue {
+
+}
+</script>
+
+<style lang='postcss' scoped>
+.brand{
+    font-family: 'Satoshi-Black';
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,53 @@
 <template>
-  <Tutorial/>
+  <main>
+    <Navbar/>
+    <section class="hero flex flex-col items-center justify-center h-screen">
+      <div class="flex flex-col items-center justify-center">
+        <h1 class="leading-3">I’m <strong class="font-bold">Yasmine</strong></h1>
+        <p class="text-sm">I create things with my keyboard !</p>
+      </div>
+      <img src="~/static/images/glasses.png" class="w-2/3 -rotate-12" alt="">
+
+      <div class="mt-5">
+        <div class="text-xl hero-text">
+          Designer <span class="text-xs hero-text--thin">&</span> Developer
+        </div>
+        <p class="text-sm text-center">with a bit of fun</p>
+      </div>
+
+      <div class="links flex items-center mt-5">
+        <a href="" class="mx-2"><img src="~/static/dribbble.svg" width="25" alt=""></a>
+        <a href="" class="mx-2"><img src="~/static/github.svg" width="25" alt=""></a>
+        <a href="" class="mx-2"><img src="~/static/email.svg" width="25" alt=""></a>
+      </div>
+
+      <p class="text-sm mt-10">Contact me via <strong class="font-bold text-black">E-mail</strong></p>
+    </section>
+  </main>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script lang='ts'>
+import {Vue, Component} from 'nuxt-property-decorator'
+import Navbar from '~/components/layout/Navbar.vue';
 
-export default Vue.extend({})
+@Component({
+  components: {
+    Navbar,
+  }
+})
+export default class Index extends Vue {
+
+}
 </script>
+
+<style lang='postcss' scoped>
+p{
+	color: #5C5C5C;
+}
+.hero-text{
+  font-family: 'Satoshi-Black';
+}
+.hero-text--thin{
+  font-family: 'Satoshi-Regular';
+}
+</style>
